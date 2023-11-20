b775fdb140c1ec1cd0698e28d4e91254e1e8a309
Luciano <20201064010039@cang217516.ifrn.local>
2023-11-20 11:43:52 -0300

    Classe animal aquatico

--- src/ReinoAnimal/Aquatico.java
+++ src/ReinoAnimal/Aquatico.java
@@ -1,20 +1,37 @@
 package ReinoAnimal;
+/**
+ * classe responsável por gerar um animal aquatico
+ * 
+ * @author 20201064010039
+ *
+ */
 
 public class Aquatico extends Animal{
 	private String tipoagua;
 	private boolean escamas;
 	
+	/**
+	 * Metódo que diz que o animal aquatico está comendo
+	 */
 	public void comer() {
 		System.out.println("O animal está aquatico esta comendo...");
 		
 	}
 
+	/**
+	 * Metódo que diz que o animal aquatico está nadando
+	 */
 	public void nadar() {
 		System.out.println("O animal está nadando...");
 		
 	}	
+	/**
+	 * Metódo que diz o perfil do animal aquatico
+	 */
+	public void perfil() {
+		System.out.println( "| escamas" + escamas+ "| tipo agua" + this.tipoagua + "especie" + this.getEspecie() + "| sexo" + this.getSexo() + "| data nasc" + this.getDataNascimento());
 	
-	
+	}
 	public String getTipoagua() {
 		return tipoagua;
 	}
