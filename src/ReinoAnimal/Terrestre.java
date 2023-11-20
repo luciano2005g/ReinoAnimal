3eddbfd1fe88aa1fb3d417a6132df91dce2bf41b
Luciano <20201064010039@cang217516.ifrn.local>
2023-11-20 11:44:29 -0300

    Classe animal terrestre

--- src/ReinoAnimal/Terrestre.java
+++ src/ReinoAnimal/Terrestre.java
@@ -1,11 +1,52 @@
 package ReinoAnimal;
 
+/**
+ * classe responsável por gerar um animal terrestre
+ * 
+ * @author 20201064010039
+ *
+ */
+
 public class Terrestre extends Animal{
 	String regiao;
 	boolean pelagem;
 	
+	/**
+	 * Metódo que diz que o animal terrestre está se movendo
+	 */
 	public void mover() {
 		System.out.println("O animal está se movendo");
 	}
+	/**
+	 * Metódo que diz que o animal terrestre está se comendo
+	 */
+	public void comer() {
+		System.out.println("O animal terrestre está comendo...");
+	}
+	/**
+	 * Metódo que diz o perfil do animal terrestre 
+	 */
+	public void perfil() {
+		System.out.println( "regiao" + this.regiao + "especie" + this.getEspecie() + "| sexo" + this.getSexo() + "| data nasc" + this.getDataNascimento());
+	
+	}
+
+	public String getRegiao() {
+		return regiao;
+	}
+
+	public void setRegiao(String regiao) {
+		this.regiao = regiao;
+	}
+
+	public boolean isPelagem() {
+		return pelagem;
+	}
+
+	public void setPelagem(boolean pelagem) {
+		this.pelagem = pelagem;
+	}
+	
+	
 
 }
